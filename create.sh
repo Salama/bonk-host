1f66f81e366e7ecff91090733e52cd1adbb1e4c2
Salama <20580796+Salama@users.noreply.github.com>
2021-11-08 10:16:22 +0200

    Fix build script

--- create.sh
+++ create.sh
@@ -5,10 +5,11 @@ version="1.4"
 description="Makes hosting rooms in bonk.io better"
 
 tmp0=$(mktemp)
-tmp1=$(mktemp)
 
 if [ ! -d build ]; then mkdir build; fi
 cd build
+rm "$sname".*
+
 csplit ../injector.js /\/\*\*\*REPLACERS\*\*\*/
 cat xx00 > "$tmp0"
 cat ../mod/replacers.js >> "$tmp0"
@@ -16,11 +17,16 @@ cat xx01 | tail -n+2 >> "$tmp0"
 csplit "$tmp0" /\/\*\*\*CONSTANTS\*\*\*/
 cat xx00 > "$tmp0"
 cat ../mod/constants.js >> "$tmp0"
-cat xx01 | tail -n+2 >> "$tmp1"
+cat xx01 | tail -n+2 >> "$tmp0"
+cp "$tmp0" injector.js
 
-cat ../manifest.json | jq ".name=\"$name\" | .version=\"$version\" | .description=\"$description\"" > "$tmp0"
+cat ../manifest.json | jq ".name=\"$name\" | .version=\"$version\" | .description=\"$description\"" > manifest.json
 
-7z a -tzip "$sname.zip" "$tmp0" "$tmp1" ../loadInjector.js ../runInjectors.js ../background.js
+cp ../loadInjector.js loadInjector.js
+cp ../runInjectors.js runInjectors.js
+cp ../background.js background.js
+
+7z a -tzip "$sname.zip" manifest.json injector.js loadInjector.js runInjectors.js background.js
 
 csplit ../userscript.js /\/\*\*\*REPLACERS\*\*\*/
 cat xx00 > "$tmp0"
@@ -31,4 +37,4 @@ cat xx00 > "$tmp0"
 cat ../mod/constants.js >> "$tmp0"
 cat xx01 | tail -n+2 >> "$tmp0"
 cat "$tmp0" | sed "s#/\*\*\*NAME\*\*\*/#$name#g" | sed "s#/\*\*\*SNAME\*\*\*/#$sname#g" | sed "s#/\*\*\*VERSION\*\*\*/#$version#g" | sed "s#/\*\*\*DESCRIPTION\*\*\*/#$description#g" > "$sname.js"
-rm xx00 xx01 "$tmp0" "$tmp1"
+rm xx00 xx01 "$tmp0" manifest.json injector.js loadInjector.js runInjectors.js background.js
