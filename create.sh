a029e78aa7b068f4ba4c51e1f898728c6269ad9f
BuildTools <unconfigured@null.spigotmc.org>
2022-01-09 23:41:19 +0200

    Bump version to 1.7

--- create.sh
+++ create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 name="Bonk Host"
 sname="bonk-host"
-version="1.6"
+version="1.7"
 description="Makes hosting rooms in bonk.io better"
 
 tmp0=$(mktemp)
@@ -14,10 +14,17 @@ csplit ../injector.js /\/\*\*\*REPLACERS\*\*\*/
 cat xx00 > "$tmp0"
 cat ../mod/replacers.js >> "$tmp0"
 cat xx01 | tail -n+2 >> "$tmp0"
+
 csplit "$tmp0" /\/\*\*\*CONSTANTS\*\*\*/
 cat xx00 > "$tmp0"
 cat ../mod/constants.js >> "$tmp0"
 cat xx01 | tail -n+2 >> "$tmp0"
+
+csplit "$tmp0" /\/\*\*\*HOSTMENU_HTML\*\*\*/
+cat xx00 > "$tmp0"
+cat ../mod/hostmenu.html >> "$tmp0"
+cat xx01 | tail -n+2 >> "$tmp0"
+
 cp "$tmp0" injector.js
 
 cat ../manifest.json | jq ".name=\"$name\" | .version=\"$version\" | .description=\"$description\"" > manifest.json
@@ -32,11 +39,19 @@ csplit ../userscript.js /\/\*\*\*REPLACERS\*\*\*/
 cat xx00 > "$tmp0"
 cat ../mod/replacers.js >> "$tmp0"
 cat xx01 | tail -n+2 >> "$tmp0"
+
 csplit "$tmp0" /\/\*\*\*CONSTANTS\*\*\*/
 cat xx00 > "$tmp0"
 cat ../mod/constants.js >> "$tmp0"
 cat xx01 | tail -n+2 >> "$tmp0"
+
+csplit "$tmp0" /\/\*\*\*HOSTMENU_HTML\*\*\*/
+cat xx00 > "$tmp0"
+cat ../mod/hostmenu.html >> "$tmp0"
+cat xx01 | tail -n+2 >> "$tmp0"
+
 cat "$tmp0" | sed "s#/\*\*\*NAME\*\*\*/#$name#g" | sed "s#/\*\*\*SNAME\*\*\*/#$sname#g" | sed "s#/\*\*\*VERSION\*\*\*/#$version#g" | sed "s#/\*\*\*DESCRIPTION\*\*\*/#$description#g" > "$sname.js"
+
 rm xx00 xx01 "$tmp0" manifest.json injector.js loadInjector.js runInjectors.js background.js
 
 chmod 400 *
